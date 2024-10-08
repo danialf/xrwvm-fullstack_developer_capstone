5b835f7aa6198f40d5e69c6e9869c3d66357045f
Danial Farzaneh <danialf@users.noreply.github.com>
2024-10-08 21:24:01 +0400

    fix stage 1

--- server/djangoapp/microservices/app.py
+++ server/djangoapp/microservices/app.py
@@ -2,16 +2,12 @@ from flask import Flask
 from nltk.sentiment import SentimentIntensityAnalyzer
 import json
 app = Flask("Sentiment Analyzer")
-
 sia = SentimentIntensityAnalyzer()
-
-
 
 @app.get('/')
 def home():
     return "Welcome to the Sentiment Analyzer. \
     Use /analyze/text to get the sentiment"
-
 
 @app.get('/analyze/<input_txt>')
 def analyze_sentiment(input_txt):
@@ -30,7 +26,6 @@ def analyze_sentiment(input_txt):
     res = json.dumps({"sentiment": res})
     print(res)
     return res
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+    
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
